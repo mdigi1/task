e48dfae35c1f6ce613929aa4ac2a666d89e796d1
maxFZ <maksym2234@gmail.com>
2021-06-03 11:44:12 +0300

    Revert "added transform"
    This reverts commit 744ad54c5e29ed7e18c7d80e4a9c0de02c59892b.

--- app/src/repo/dto/save-repo.dto.ts
+++ app/src/repo/dto/save-repo.dto.ts
@@ -1,5 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
 import { IsDate, IsOptional, IsString } from 'class-validator'
 
 export class SaveRepoDto {
